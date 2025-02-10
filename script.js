39f9561099201e43036a69608bbe9ae4e8f96344
Dhanish kumar <dhanishmca1@gmail.com>
2025-02-10 11:25:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+s = s.trim();
+    const spaceIndex = s.indexOf(" ");
+
+    // If no space is found, return the entire string
+    return spaceIndex === -1 ? s : s.slice(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s));;
